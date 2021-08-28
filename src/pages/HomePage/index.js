cb1e45b65df38d03d32ead821434a52111bfc4c9
axdr3 <axdber@gmail.com>
2021-08-28 12:19:41 -0300

    added homepage

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+
+import Header from "../../components/Header";
+
+import { Link, useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  nav: {
+    display: "flex",
+    alignItems: "center",
+    justifyItems: "center",
+    justifyContent: "center",
+    "& > *": {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+const HomePage = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  return <Container maxWidth={"sm"}>Homepage</Container>;
+};
+
+export default HomePage;
